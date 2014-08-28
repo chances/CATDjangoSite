2ae39d89af11ca81baf4b6abb7aa130daeda481f
Brian Breniser <brianbreniser@gmail.com>
2014-08-28 16:09:36 -0700

    can almost add a new item now (zuulmaster only!)

--- newzuul/urls.py
+++ newzuul/urls.py
@@ -7,11 +7,31 @@ urlpatterns = patterns('',
     #index, displays everyones name and bank balance
     url(r'^$', views.index, name='index'),
 
-    #a purchase page, will display optoins for user to purchase
+    #a purchase page, will display options for user to purchase
     url(r'^(?P<user_id>\d+)/purchase/$', views.purchaselist,
          name='purchaselist'),
 
-    #purchase one item, for one user
+    #purchase lots-o-items for one user
     url(r'^(?P<user_id>\d+)/purchase/purchaseaction/$', views.purchaseaction,
         name='purchaseaction'),
+
+
+
+    #Add an item page
+    url(r'^additem/$', views.additemform,
+        name='additemform'),
+
+    #Add an item action page
+    url(r'^additemaction/$', views.additemaction,
+        name='additemaction'),
+
+
+
+    #Add a user page
+    url(r'^adduser/$', views.adduserform,
+        name='adduserform'),
+
+    #Add a user page
+    url(r'^adduseraction/$', views.adduseraction,
+        name='adduseraction'),
 )
--- newzuul/views.py
+++ newzuul/views.py
@@ -16,7 +16,7 @@ def purchase_item(person, item_id, ammount):
         return True
 
 
-def creat_item(name_in, price_in):
+def create_item(name_in, price_in):
     new_item = items(name=name_in, price=price_in)
     new_item.save()
 
@@ -27,6 +27,7 @@ def create_consumer(name_in, bank_in):
 
 
 def index(request):
+# show all people and all items (include banks and costs respectively)
     consumer_list = consumer.objects.order_by('name')
     item_list = items.objects.order_by('price')
     context = {'consumer_list': consumer_list,
@@ -35,6 +36,7 @@ def index(request):
 
 
 def purchaselist(request, user_id):
+# purchaselist for person
     person = get_object_or_404(consumer, pk=user_id)
     item_list = items.objects.order_by('price')
     context = {'item_list': item_list, 'person': person}
@@ -42,14 +44,42 @@ def purchaselist(request, user_id):
 
 
 def purchaseaction(request, user_id):
+# action page for purchases, redirects to index
     try:
         person = get_object_or_404(consumer, pk=user_id)
     except (ValueError):
         return HttpResponse(" he's dead Jim (ValueError on person object)")
     else:
         for key in request.POST:
-            purchase_item(person, key, request.POST[key]):
+            if key == "csrfmiddlewaretoken":
+                continue
+            elif purchase_item(person, key, request.POST[key]):
+                continue
+            else:
                 #print exception error on webpage
-                return HttpResponse('Hes dead Jim (purchase_item f)')
+                return HttpResponse('Hes dead Jim (purchase_item function ValueError)')
         #return HttpResponse('did it!')
         return redirect('newzuul:purchaselist.html')  # causes 400 error on runserver
+
+
+
+
+
+
+def additemform(request):
+    return render(request, 'newzuul/additemform.html')
+
+
+def additemaction(request):
+    return HttpResponse('you found me!')
+
+
+
+
+def adduserform(request):
+    return HttpResponse('you found me!')
+
+
+def adduseraction(request):
+    return HttpResponse('you found me!')
+
