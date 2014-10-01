a24eb6f8b5639e5317fb1a273bf916073ad0850b
brianbreniser <brianbreniser@gmail.com>
2014-10-01 14:41:04 -0700

    settings change

--- catsite/publicsettings.py
+++ catsite/publicsettings.py
@@ -17,7 +17,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'c#h+0kvxb9h$sbr^6&wgam$lwd+-vs3sqw)-8&^4hy92n-vjwa'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
