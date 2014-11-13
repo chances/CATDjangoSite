d4b5fef4affdac81648f904ae88975122b10c08c
peter <pkeoman17@gmail.com>
2014-11-12 22:08:46 -0800

    implement db models

--- doglog/models.py
+++ doglog/models.py
@@ -15,5 +15,46 @@ class person(models.Model):
     mcecs_username = models.CharField(max_length=50)
     pdx_username = models.CharField(max_length=50)
     cat_username = models.CharField(max_length=50)
+    cat_handle  = models.CharField(max_length=50)
     preferred_email = models.CharField(max_length=50)
+    number_of_missed_mettings = models.IntegerField(default = 0)
+    number_of_no_call = models.IntegerField(default = 0)
+    strikes = models.IntegerField(default = 0)
+    availability_flag =  models.BooleanField(default=False)
+    dog_flag = models.BooleanField(default= True)
+    dedog_flag = models.BooleanField(default = False)
+    archived_dog_flag = models.BooleanField(default = False)
+    alt_dog_flag = models.BooleanField(default = False)
+    notes = models.CharField(max_length =140)
+    num_of_absent_hr = models.IntegerField(default = 0)
+    num_of_made_up_hr = models.IntegerField(default = 0)
+    num_of_no_call_hr = models.IntegerField(default = 0)
 
+class shift(models.Model):
+    user_id = models.ForeignKey(person)
+
+    start_date = models.DateField()
+    start_time = models.TimeField()
+
+    end_date = models.DateField()
+    end_time = models.TimeField()
+    expiry_date = models.DateField()
+
+    dropped_flag = models.BooleanField(default = False)
+    missed_flag = models.BooleanField(default = False)
+
+class Availability(models.Model):
+    user_id = models.ForeignKey(person)
+
+
+    start_date = models.DateField()
+    start_time = models.TimeField()
+
+    end_date = models.DateField()
+    end_time = models.TimeField()
+
+    effective_date = models.DateField()
+
+class holiday(models.Model):
+    name = models.CharField(max_length = 40)
+    date = models.DateField()
