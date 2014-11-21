dae6cd5c862c5380064c941c739526e12f2cbf10
peter <pkeoman17@gmail.com>
2014-11-20 23:50:30 -0800

    template of the urls and views

--- doglog/urls.py
+++ doglog/urls.py
@@ -4,5 +4,16 @@ from django.conf.urls import patterns, url
 from newzuul import views
 
 urlpatters = patterns('',
-    url(r'^$', views.index, name='index')
+
+    url(r'^$', views.index, name='index'),
+
+    url(r'^(?P<user_id>\d+/)/availability/$', views.availability, name='availability'),
+
+    url(r'^(?P<user_id>\d+/)/profile/$', views.profile, name='profile'),
+
+    url(r'^(?P<user_id>\d+/)/admin/$', views.admin, name='admin'),
+
+
+    url(r'^(?P<user_id>\d+/)/available_shifts/$', views.available_shift,
+        name='available_shifts'),
     )
--- doglog/views.py
+++ doglog/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here
+import logging
 
-# Create your views here.
+logger = logging.getLogger(__name__)
+
+def availability():
+    logger.info("Not implemented yet")
+
+def profile():
+
+    logger.info("Not implemented yet")
+
+def admin():
+
+    logger.info("Not implemented yet")
+
+def available_shifts():
+
+    logger.info("Not implemented yet")
